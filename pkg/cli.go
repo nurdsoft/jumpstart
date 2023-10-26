d1e5eaab0a3bc4f8941a49a952f674e44594e130
rogdevil <sep2001aryan@yahoo.com>
2023-10-26 10:21:17 +0530

    fixes suggested

--- pkg/cli.go
+++ pkg/cli.go
@@ -17,16 +17,12 @@ var (
 
 func NewApp() *cli.App {
 	return &cli.App{
-		Name:      "jumpstart",
-		Usage:     "jumpstart your project",
-		UsageText: "jumpstart command [command option]",
-		Version:   VERSION + "-" + COMMIT + "+" + BUILDTIME,
+		Name:    "jumpstart",
+		Usage:   "jumpstart your project",
+		Version: VERSION + "-" + COMMIT + "+" + BUILDTIME,
 		Commands: []*cli.Command{
 			{
-				Name:        "template",
-				UsageText:   "jumpstart template [sub-command]",
-				Usage:       "Perform template related actions e.g. List, Sync.",
-				Description: "Use this to list the templates supported and sync the supported templates with remote",
+				Name: "template",
 				Subcommands: []*cli.Command{
 					{
 						Name:  "list",
@@ -56,70 +52,70 @@ func NewApp() *cli.App {
 					},
 				},
 			},
-			{
-				Name:      "create",
-				Usage:     "Create a new project with the specified supporeted template",
-				UsageText: "jumpstart create -t={supported template name} [project-name]",
-				Flags: []cli.Flag{
-					&cli.StringFlag{
-						Name:    "template",
-						Aliases: []string{"t"},
-						Usage:   "template to use",
-					},
-				},
-				Action: func(c *cli.Context) error {
-					// Check if default template directory exists
-					_, err := os.Stat(absPath(DEFAULT_TEMPLATES_DIR))
-					if os.IsNotExist(err) {
-						fmt.Printf("Please run `jumpstart template sync` to seed templates\n\n")
-						os.Exit(2)
-					}
-
-					tid := c.String("template")
-					if tid == "" {
-						fmt.Printf("-t | --template is required\n\n")
-						cli.ShowAppHelp(c)
-						os.Exit(2)
-					}
-
-					dm, err := NewDerivedMetadata(c.Context, c.Args().First())
-					if err != nil {
-						return err
-					}
-					logrus.Infof("derived metadata: %+v", *dm)
-
-					sshKeys := NewSSHKeys()
-					{
-						count := len(sshKeys.List())
-						if count == 0 {
-							fmt.Println("No SSH keys found! Please generate one and try again!")
-							fmt.Println("See https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent for more information")
-							os.Exit(2)
-						} else if count > 1 {
-							fmt.Printf("Multiple SSH keys found! Please select one to use:\n\n")
-							for i, k := range sshKeys.List() {
-								fmt.Printf("%d. %s\n", i+1, k)
-							}
-							fmt.Printf("\nEnter selection: ")
-
-							// Read selection from user
-							var selection int
-							fmt.Scanln(&selection)
-							sshKeys.SetSelected(selection - 1)
-						}
-					}
-					fmt.Printf("\nUsing ssh key: %s\n", sshKeys.Get())
-					fmt.Print("Enter ssh passphrase if you added it while ssh-key generation: ")
-					var passphrase string
-					fmt.Scanln(&passphrase)
-					gitSSHKeys, err := ssh.NewPublicKeysFromFile("git", sshKeys.Get(), passphrase)
-					if err != nil {
-						return err
-					}
-
-					return SynthesizeProject(c.Context, tid, dm, gitSSHKeys)
-				},
+		},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "template",
+				Aliases: []string{"t"},
+				Usage:   "template to use",
 			},
+		},
+		Action: func(c *cli.Context) error {
+			// Check if default template directory exists
+			_, err := os.Stat(absPath(DEFAULT_TEMPLATES_DIR))
+			if os.IsNotExist(err) {
+				fmt.Printf("Please run `jumpstart template sync` to seed templates\n\n")
+				os.Exit(2)
+			}
+
+			tid := c.String("template")
+			if tid == "" {
+				fmt.Printf("-t | --template is required\n\n")
+				cli.ShowAppHelp(c)
+				os.Exit(2)
+			}
+
+			dm, err := NewDerivedMetadata(c.Context, c.Args().First())
+			if err != nil {
+				return err
+			}
+			logrus.Infof("derived metadata: %+v", *dm)
+
+			sshKeys := NewSSHKeys()
+			{
+				count := len(sshKeys.List())
+				if count == 0 {
+					fmt.Println("No SSH keys found! Please generate one and try again!")
+					fmt.Println("See https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent for more information")
+					os.Exit(2)
+				} else if count > 1 {
+					fmt.Printf("Multiple SSH keys found! Please select one to use:\n\n")
+					for i, k := range sshKeys.List() {
+						fmt.Printf("%d. %s\n", i+1, k)
+					}
+					fmt.Printf("\nEnter selection: ")
+
+					// Read selection from user
+					var selection int
+					fmt.Scanln(&selection)
+					sshKeys.SetSelected(selection - 1)
+				}
+			}
+			fmt.Printf("\nUsing ssh key: %s\n", sshKeys.Get())
+			var passphrase string
+			ghSSHPassphrase, isGhSSHPassphrase := os.LookupEnv("GITHUB_SSH_PASSPHRASE")
+			if isGhSSHPassphrase {
+				passphrase = ghSSHPassphrase
+			} else {
+				fmt.Print("Enter ssh passphrase if you added it while ssh-key generation: ")
+				fmt.Scanln(&passphrase)
+			}
+			gitSSHKeys, err := ssh.NewPublicKeysFromFile("git", sshKeys.Get(), passphrase)
+			if err != nil {
+				return err
+			}
+
+			return SynthesizeProject(c.Context, tid, dm, gitSSHKeys)
 		},
 	}
 }
