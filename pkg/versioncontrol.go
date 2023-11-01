104f45ec7db0a9292d6c017a44675e33ebe74312
Darshan <er.darshanghetiya@gmail.com>
2023-11-01 12:12:53 +0530

    fix: removed the unwanted code

--- pkg/versioncontrol.go
+++ pkg/versioncontrol.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"time"
 
-	"github.com/go-git/go-git"
-	"github.com/go-git/go-git/config"
-	"github.com/go-git/go-git/plumbing/object"
-	"github.com/go-git/go-git/plumbing/transport/http"
-	"github.com/google/go-github/github"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+
+	"github.com/google/go-github/v56/github"
 )
 
 const (
@@ -40,6 +42,7 @@ func VersionControl(dir, repoName string) (*git.Repository, error) {
 		Author: &object.Signature{
 			Name:  "Jump Start",
 			Email: "jumpstart",
+			When:  time.Now(),
 		},
 	})
 	if err != nil {
@@ -52,62 +55,6 @@ func VersionControl(dir, repoName string) (*git.Repository, error) {
 // namespace should be empty if it is not an org
 func SetupGithubRemote(ctx context.Context, namespace string, repoName string, localRepo *git.Repository, push bool) error {
 	// Create github repo
-
-	// tokens := &oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")}
-	// ts := oauth2.StaticTokenSource(tokens)
-	// tc := oauth2.NewClient(ctx, ts)
-
-	// client := github.NewClient(tc)
-
-	// auth := &http.BasicAuth{
-	// 	Username: "Darshan072",
-	// 	Password: os.Getenv("GITHUB_TOKEN"),
-	// }
-
-	// newR, _, _ := client.Repositories.Get(ctx, "Darshan072", repoName)
-
-	// if newR != nil {
-	// 	return errors.New("repository already exists")
-	// }
-
-	// repo := &github.Repository{
-	// 	Name: github.String(repoName),
-	// }
-
-	// result, _, err := client.Repositories.Create(ctx, namespace, repo)
-
-	// if err != nil {
-	// 	return err
-	// }
-
-	// repository := "https://github.com/" + "Darshan072" + "/" + repoName + ".git"
-	// if result != nil {
-	// 	repository = *result.CloneURL
-	// }
-	// // localRepo.CreateRemote(&)
-
-	// newRepo, err := locak.PlainClone(newClonePath, false, &git.CloneOptions{
-	// 	URL:        repository,
-	// 	RemoteName: "origin/main",
-	// 	Auth:       auth,
-	// })
-
-	// if err != nil {
-	// 	return err
-	// }
-
-	// err = localRepo.Push(&git.PushOptions{
-	// 	RemoteName: "origin/main",
-	// 	Auth:       auth,
-	// })
-
-	// if err != nil {
-	// 	log.Printf("ERR: final commit newRepo push event")
-	// 	return err
-	// }
-
-	/////////
-
 	ghRepo, err := NewGithubAPI().CreateRepo(ctx, namespace, repoName)
 	if err != nil {
 		return err
@@ -129,7 +76,7 @@ func SetupGithubRemote(ctx context.Context, namespace string, repoName string, l
 		err = localRepo.Push(&git.PushOptions{
 			RemoteName: "origin",
 			Auth: &http.BasicAuth{
-				Username: "Darshan072",
+				Username: "x-access-token",
 				Password: os.Getenv("GITHUB_TOKEN"),
 			},
 		})
